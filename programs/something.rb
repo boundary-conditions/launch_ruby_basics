3daae459790ccab5770c7aa05d39a1e5c92dbe88
Charles Keenan <Keenan.Charlese@gmail.com>
2020-06-19 12:36:14 -0300

    Expanding the user input to require a valid response, without case sensitivity

--- programs/something.rb
+++ programs/something.rb
@@ -1,3 +1,31 @@
-puts "Would you like me to print something? y/n"
-user_response = gets.chomp
-puts "Something" if user_response == "y"
+# puts "Would you like me to print something? y/n"
+# user_response = gets.chomp
+# user_response.downcase!
+
+# loop do
+#   if user_response == "y"
+#     puts "Something"
+#     break
+#   elsif user_response == "n"
+#     break
+#   else
+#   puts "Error, please enter y or n. Would you like me to print something?"
+#   user_response = gets.chomp
+#   user_response.downcase!
+# end
+# end
+
+#puts "Something" if user_response == "y"
+
+#LS solution below
+
+user_response = nil
+loop do
+  puts "Would you like me to print something?"
+  user_response = gets.chomp.downcase
+  break if %w(y n).include?(user_response)
+  puts "Error, please enter y or n"
+end
+puts "Something" if user_response == 'y'
+
+
